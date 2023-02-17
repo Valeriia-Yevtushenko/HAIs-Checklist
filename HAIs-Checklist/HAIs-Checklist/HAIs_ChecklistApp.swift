8fadd25c084dfef26c5ed01542110fd24ff81477
Valeriia Yevtushenko <yevtushenko.v.y@gmail.com>
2023-02-17 16:55:30 +0100

    add initial project

---
+++ HAIs-Checklist/HAIs-Checklist/HAIs_ChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  HAIs_ChecklistApp.swift
+//  HAIs-Checklist
+//
+//  Created by Valeriia Yevtushenko on 17.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct HAIs_ChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
